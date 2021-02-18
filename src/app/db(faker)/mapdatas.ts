7db05f5408c9355ac9db337370c5c74cd3bc37be
kumar vadivel <kumarvadivel9443493672@gmail.com>
2021-02-18 18:59:54 +0530

    update9

---
+++ src/app/db(faker)/mapdatas.ts
@@ -0,0 +1,406 @@
+const mapdatas=[
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      location:"delhi",
+      locationcode:"1000011",
+      from:"12-1-2020",
+      to:"2-3-2020",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      location:"delhi",
+      locationcode:"1000011",
+      from:"12-1-2020",
+      to:"2-3-2020",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      location:"delhi",
+      locationcode:"1000011",
+      from:"12-1-2020",
+      to:"2-3-2020",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      location:"delhi",
+      locationcode:"1000011",
+      from:"12-1-2020",
+      to:"2-3-2020",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      location:"delhi",
+      locationcode:"1000011",
+      from:"12-1-2020",
+      to:"2-3-2020",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      location:"delhi",
+      locationcode:"1000011",
+      from:"12-1-2020",
+      to:"2-3-2020",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      location:"delhi",
+      locationcode:"1000011",
+      from:"12-1-2020",
+      to:"2-3-2020",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      location:"delhi",
+      locationcode:"1000011",
+      from:"12-1-2020",
+      to:"2-3-2020",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      location:"delhi",
+      locationcode:"1000011",
+      from:"12-1-2020",
+      to:"2-3-2020",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      location:"delhi",
+      locationcode:"1000011",
+      from:"12-1-2020",
+      to:"2-3-2020",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    
+  ]
+
+  export default mapdatas
+  
---
+++ src/app/db(faker)/unallocatedimds.ts
@@ -0,0 +1,365 @@
+const mapdatas=[
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+    {
+      imdcode:100011,
+      imdname:"Malav Patel",
+      amcode:13425,
+      amname:"john doe",
+      pbd:"Smart Insurance",
+      codename:"Smart Insurance",
+      digitlead:"Chandra sekhar sharma",
+      rmname:"ruban",
+      rmsts:"Registered",
+      city:"Bangalore",
+      team:"Bangalore-team",
+      type:"N/A",
+      rmcode:16542,
+      imdsts:"Active",
+      imdmstrcity:"Mumbai",
+      channel:["Motor","Multiline","Insurance"],
+      zone:"North-1",
+      versionhistory:[
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        },
+        {
+          details:"Employee channel changed from Broker to Agency",
+          datetime:"31st Dec 2020 at 5:15pm",
+          modifiedby:"john doe"
+        }
+      ]
+    },
+  ]
+
+
+  export default mapdatas;
