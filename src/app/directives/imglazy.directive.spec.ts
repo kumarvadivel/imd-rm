2c17405cea6b2a44a66c5b152b038e10ee2d0818
kumar vadivel <kumarvadivel9443493672@gmail.com>
2021-02-26 11:31:00 +0530

    small css fix

---
+++ src/app/directives/imglazy.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ImglazyDirective } from './imglazy.directive';
+
+describe('ImglazyDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ImglazyDirective();
+    expect(directive).toBeTruthy();
+  });
+});
